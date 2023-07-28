0fe1d1ef5898387dab7a1aeea3b8df0cf18e2583
Yader Velasquez <yajosev@gmail.com>
2023-07-27 19:31:38 -0600

    Basic project structure and db schema

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/yaderv/medusario
+
+go 1.20
---
+++ schema.sql
@@ -0,0 +1,31 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR ( 150 ) NOT NULL,
+    password VARCHAR ( 1024 ) NOT NULL,
+    email VARCHAR ( 255 ) UNIQUE NOT NULL,
+);
+
+CREATE INDEX users_id_idx ON users (id);
+
+CREATE courses (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR ( 150 ) NOT NULL,
+    start_date TIMESTAMPTZ,
+    is_active BOOLEAN DEFAULT TRUE,
+    user_id REFERENCES users (id) ON DELETE CASCADE NOT NULL,
+);
+
+CREATE INDEX courses_id_idx ON courses (id);
+CREATE INDEX courses_user_id_idx ON courses (user_id);
+
+CREATE lessons (
+    id SERIAL PRIMARY KEY,
+    topic VARCHAR ( 150 ) NOT NULL,
+    num INT NOT NULL DEFAULT 0,
+    text TEXT NOT NULL,
+    course_id REFERENCES courses (id) ON DELETE
+
+);
+
+CREATE INDEX lessons_id_idx ON lessons (id);
+CREATE INDEX lessons_course_id_idx ON lessons (course_id);
