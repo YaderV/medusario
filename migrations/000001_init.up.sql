6ab8655208d78cc35a48e607f3445a3dc1e3bf0d
Yader Velasquez <yajosev@gmail.com>
2023-08-31 19:50:54 -0600

    Support migrations in docker

--- migrations/000001_init.up.sql
+++ migrations/000001_init.up.sql
@@ -1,8 +1,8 @@
-CREATE locations (
+CREATE TABLE locations (
     id SERIAL PRIMARY KEY,
     title VARCHAR (50) NOT NULL,
     description TEXT NOT NULL,
-    address VARCHAR(140) NOT NULL,
+    address VARCHAR(140) NOT NULL
 );
 
 CREATE INDEX locations_id_idx ON locations (id);
