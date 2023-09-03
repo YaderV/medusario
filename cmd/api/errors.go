50d1cd9311da4b39f15fb576dd897c38c5e82536
Yader Velasquez <yajosev@gmail.com>
2023-09-02 18:25:11 -0600

    Add helpers to manage json errors

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -2,11 +2,15 @@ package main
 
 import "net/http"
 
-func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, msg string) {
+func (app *application) errorResponse(w http.ResponseWriter, status int, msg string) {
 	env := envelope{"error": msg}
 	err := app.writeJSON(w, status, env, nil)
 	if err != nil {
 		app.logger.Println(err)
 		w.WriteHeader(http.StatusInternalServerError)
 	}
+}
+
+func (app *application) badRequestResponse(w http.ResponseWriter, msg string) {
+	app.errorResponse(w, http.StatusBadRequest, msg)
 }
--- cmd/api/locations.go
+++ cmd/api/locations.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 
 	"github.com/yaderv/medusario/internal/data"
@@ -9,9 +8,15 @@ import (
 
 func (app *application) createLocationHandler(w http.ResponseWriter, r *http.Request) {
 	var loc data.Location
-	err := json.NewDecoder(r.Body).Decode(&loc)
+	err := app.readJSON(r, loc)
 	if err != nil {
-		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		app.errorResponse(w, http.StatusBadRequest, err.Error())
+		return
 	}
-	app.models.Locations.Insert(&loc)
+	//app.models.Locations.Insert(&loc)
+	err = app.writeJSON(w, http.StatusOK, loc, nil)
+	if err != nil {
+		app.errorResponse(w, http.StatusBadRequest, err.Error())
+		return
+	}
 }
