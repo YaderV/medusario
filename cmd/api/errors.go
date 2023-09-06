f5f848959cc84e18e9653f0eeab694beff1124c9
Yader Velasquez <yajosev@gmail.com>
2023-09-06 17:03:43 -0600

    Improve failed validation msg

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -1,9 +1,13 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
 
-func (app *application) errorResponse(w http.ResponseWriter, status int, msg string) {
-	env := envelope{"error": msg}
+	"github.com/go-playground/validator/v10"
+)
+
+func (app *application) errorResponse(w http.ResponseWriter, status int, errors any) {
+	env := envelope{"error": errors}
 	err := app.writeJSON(w, status, env, nil)
 	if err != nil {
 		app.logger.Println(err)
@@ -13,4 +17,13 @@ func (app *application) errorResponse(w http.ResponseWriter, status int, msg str
 
 func (app *application) badRequestResponse(w http.ResponseWriter, msg string) {
 	app.errorResponse(w, http.StatusBadRequest, msg)
+}
+
+func (app *application) failedValidationResponse(w http.ResponseWriter, err error) {
+	errorMap := make(map[string]string)
+	for _, err := range err.(validator.ValidationErrors) {
+		// NOTE: this should be improved and return the json tag name instead
+		errorMap[err.Field()] = err.Tag()
+	}
+	app.errorResponse(w, http.StatusBadRequest, errorMap)
 }
--- cmd/api/locations.go
+++ cmd/api/locations.go
@@ -10,18 +10,18 @@ func (app *application) createLocationHandler(w http.ResponseWriter, r *http.Req
 	var loc data.Location
 	err := app.readJSON(w, r, &loc)
 	if err != nil {
-		app.errorResponse(w, http.StatusBadRequest, err.Error())
+		app.badRequestResponse(w, err.Error())
 		return
 	}
 	err = loc.Validate()
 	if err != nil {
-		app.errorResponse(w, http.StatusBadRequest, err.Error())
+		app.failedValidationResponse(w, err)
 		return
 	}
 	//app.models.Locations.Insert(&loc)
 	err = app.writeJSON(w, http.StatusOK, loc, nil)
 	if err != nil {
-		app.errorResponse(w, http.StatusBadRequest, err.Error())
+		app.badRequestResponse(w, err.Error())
 		return
 	}
 }
