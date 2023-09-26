259c87eeb8fe8c82bddc6bea65d0f4af8626caa7
Yader Velasquez <yajosev@gmail.com>
2023-09-26 11:20:52 -0600

    Add swagger and redoc documentation

--- locations/cmd/api/routes.go
+++ locations/cmd/api/routes.go
@@ -4,10 +4,23 @@ import (
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
+	"github.com/mvrilo/go-redoc"
 )
+
+var doc = redoc.Redoc{
+	Title:       "Location API",
+	Description: "Location API provides endpoints to manage house or appartment with renting space.",
+	SpecFile:    "./swagger.yaml",
+	SpecPath:    "/v1/swagger.yaml",
+	DocsPath:    "/v1/docs",
+}
 
 func (app *application) routes() *httprouter.Router {
 	router := httprouter.New()
+	router.Handler(http.MethodGet, "/v1/docs", doc.Handler())
+	router.HandlerFunc(http.MethodGet, "/v1/swagger.yaml", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./swagger.yaml")
+	})
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/locations", app.createLocationHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/locations", app.showLocationListHandler)
--- locations/go.mod
+++ locations/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/go-playground/validator/v10 v10.15.3
 	github.com/julienschmidt/httprouter v1.3.0
 	github.com/lib/pq v1.10.9
+	github.com/mvrilo/go-redoc v0.1.4
 	github.com/stretchr/testify v1.8.4
 )
 
@@ -16,9 +17,9 @@ require (
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	golang.org/x/crypto v0.7.0 // indirect
-	golang.org/x/net v0.8.0 // indirect
-	golang.org/x/sys v0.6.0 // indirect
-	golang.org/x/text v0.8.0 // indirect
+	golang.org/x/crypto v0.9.0 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
