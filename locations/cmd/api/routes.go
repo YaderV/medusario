cfcb41a7814f00ccba1adfc5ec4f25dac0afa663
Yader Velasquez <yajosev@gmail.com>
2024-01-02 18:07:18 -0600

    Improve project structure and docker files

--- locations/cmd/api/routes.go
+++ locations/cmd/api/routes.go
@@ -10,7 +10,7 @@ import (
 var doc = redoc.Redoc{
 	Title:       "Location API",
 	Description: "Location API provides endpoints to manage house or appartment with renting space.",
-	SpecFile:    "./swagger.yaml",
+	SpecFile:    "swagger.yaml",
 	SpecPath:    "/v1/swagger.yaml",
 	DocsPath:    "/v1/docs",
 }
@@ -19,7 +19,7 @@ func (app *application) routes() *httprouter.Router {
 	router := httprouter.New()
 	router.Handler(http.MethodGet, "/v1/docs", doc.Handler())
 	router.HandlerFunc(http.MethodGet, "/v1/swagger.yaml", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./swagger.yaml")
+		http.ServeFile(w, r, "swagger.yaml")
 	})
 	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/locations", app.createLocationHandler)
